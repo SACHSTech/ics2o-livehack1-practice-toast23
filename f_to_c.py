4a6dfc7d0eebbf5a47117b56ee4c6be2f3bcd0af
Tony <tony.yao23@ycdsbk12.ca>
2021-02-08 14:49:37 +0000

    completed f_to_c.py

--- f_to_c.py
+++ f_to_c.py
@@ -1,1 +1,19 @@
+"""
+-------------------------------------------------------------------------------
+Name:   f_to_c.py
+Purpose:  Convert fahrenheit to celsius
+ 
+Author: Yao.T
+ 
+Created:  08/02/2021
+------------------------------------------------------------------------------
+"""
 
+#get degrees Fahrenheit
+degreeF = float(input("Degrees in Fahrenheit: "))
+
+#convert Fahrentheit to Celsius
+degreeC = (degreeF - 32) / 1.8
+
+#output
+print(f"Degrees in Celsius: {round(degreeC,2)}")
