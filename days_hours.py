bc88427370fcf46fdff615fe6bf4e2d0b8fff19e
Tony <tony.yao23@ycdsbk12.ca>
2021-02-08 15:01:51 +0000

    completed days_hours.py

--- days_hours.py
+++ days_hours.py
@@ -1,1 +1,17 @@
+"""
+-------------------------------------------------------------------------------
+Name:   filename.py
+Purpose:  a description of your program
+ 
+Author: lastname.First Initial
+ 
+Created:  date in dd/mm/yyyy
+------------------------------------------------------------------------------
+"""
+
+#get hours
+hours = int(input("Enter hours: "))
+
+#convert to days and hours + output 
+print(f"{int(hours / 24)} days and {hours % 24} hours")
 
